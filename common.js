9acfb294b3373a24ffe4b81895b0f791561ddac7
DESKTOP-M29N2EE\test <bh5322@naver.com>
2022-06-27 19:42:41 +0900

    [박유진] 숫자카운트 애니메이션

--- common.js
+++ common.js
@@ -1,1 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
+
+
+//숫자 카운트다운
+var startCount = {var: 0};
+
+gsap.to(startCount, {
+    var:1000, duration: 3, ease:"none",
+    onUpdate:changeNumber,
+    scrollTrigger:{
+        trigger:"#number",
+        toggleActions:"restart none reverse none",
+    },
+})
+
+var Number = document.querySelector('#number');
+
+function changeNumber(){
+    Number.innerHTML = (startCount.var).toFixed();
+}
